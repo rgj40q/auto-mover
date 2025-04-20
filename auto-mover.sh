cb8661e959f672342a03a275f7098fd313bafaaa
Margarito Tengben <45658038+rgj40q@users.noreply.github.com>
2025-04-20 12:49:21 +0700

    Update auto-mover.sh

--- auto-mover.sh
+++ auto-mover.sh
@@ -2,7 +2,7 @@
 
 for disk in L M N O P Q R; do
 	avail=$(( $(df --output=source,avail | grep "$disk:" | cut -d ':' -f 2) / 1048576 ))
-	if [[ $avail -gt 2 ]]; then
+	if [[ $avail -gt 1 ]]; then
 		size=$(( avail - 1 ))
 		pwsh 'A:\RT_Pack-main\Mover.ps1' -category '' -max_1_size $size -max_size $size -id_subfolder Y -client_name client2 -min_move_days 30 -path_from 'S:\j' -path_to "$disk:\\j"
 	fi
