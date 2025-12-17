1c69a72abed9edd0917c8febaf0b8134c17aeff1
Margarito Tengben <45658038+rgj40q@users.noreply.github.com>
2025-12-17 19:19:29 +0700

    Add files via upload

--- auto-mover.sh
+++ auto-mover.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-for disk in L M N O P Q R; do
+for disk in L M N O P R S; do
 	avail=$(( $(df --output=source,avail | grep "$disk:" | cut -d ':' -f 2) / 1048576 ))
 	if [[ $avail -gt 1 ]]; then
-		size=$(( avail - 1 ))
-		pwsh 'A:\RT_Pack-main\Mover.ps1' -category '' -max_1_size $size -max_size $size -id_subfolder Y -client_name client2 -min_move_days 30 -path_from 'S:\j' -path_to "$disk:\\j"
+		pwsh 'A:\RT_Pack-main\Mover.ps1' -category '' -max_1_size $(($avail - 1)) -max_size $(($avail - 1)) -id_subfolder Y -client_name client1 -min_move_days 0 -path_from 'T:\r' -path_to "$disk:\\r"
 	fi
 done
--- avail.sh
+++ avail.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+while true; do
+
 gb=0
 
-for disk in D L M N O P Q R S; do
+for disk in L M N O P S T; do
 	avail=$(( $(df --output=source,avail | grep "$disk:" | cut -d ':' -f 2) / 1048576 ))
 	gb=$((gb + avail))
 done
 
 tb=$(echo $((gb * 100 / 1024)) | sed 's/..$/.&/')
 
-echo "$gb GB = $tb TB"
+echo "$(date +%H:%M:%S) $gb GB = $tb TB"
+
+sleep 5
+done
